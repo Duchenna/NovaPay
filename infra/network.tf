750209c531b80a514e0f66ffcbfc86ec7d9c6341
Duchenna <dimakunne.uchenna@gmail.com>
2026-09-19 22:14:30 +0100

    waf_updated

--- infra/network.tf
+++ infra/network.tf
@@ -355,3 +355,24 @@ resource "aws_wafv2_web_acl_association" "wallet" {
   resource_arn = aws_lb.wallet.arn
   web_acl_arn  = aws_wafv2_web_acl.wallet.arn
 }
+rule {
+  name     = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
+  priority = 2
+
+  override_action {
+    none {}
+  }
+
+  statement {
+    managed_rule_group_statement {
+      name        = "AWSManagedRulesKnownBadInputsRuleSet"
+      vendor_name = "AWS"
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "novapay-log4j-protection"
+    sampled_requests_enabled   = true
+  }
+}
