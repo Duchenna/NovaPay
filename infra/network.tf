84e20b296fdd3027b69bbada190888f75c7eb686
Duchenna <dimakunne.uchenna@gmail.com>
2026-09-19 08:14:55 +0000

    network_file_updated

--- infra/network.tf
+++ infra/network.tf
@@ -49,35 +49,4 @@ resource "aws_db_subnet_group" "private" {
   }
 }
 
-resource "aws_iam_role_policy" "task" {
-  role = aws_iam_role.task.id
-  policy = jsonencode({
-    Statement = [{
-      Effect   = "Allow"
-      Action   = ["secretsmanager:GetSecretValue"]
-      Resource = aws_secretsmanager_secret.db_pass.arn
-    }]
-  })
-}
 
-resource "aws_iam_role_policy" "task_exec" {
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      { Effect = "Allow"
-        Action = ["ecr:GetAuthorizationToken"]
-      Resource = "*" }, # JUSTIFIED: account-scoped, AWS-defined, no narrower ARN exists
-      { Effect = "Allow"
-        Action = ["ecr:BatchCheckLayerAvailability",
-          "ecr:GetDownloadUrlForLayer",
-        "ecr:BatchGetImage"]
-      Resource = aws_ecr_repository.wallet.arn },
-      { Effect = "Allow"
-        Action = ["logs:CreateLogStream", "logs:PutLogEvents"]
-      Resource = "${aws_cloudwatch_log_group.wallet.arn}:*" },
-      { Effect = "Allow"
-        Action = ["secretsmanager:GetSecretValue"]
-      Resource = aws_secretsmanager_secret.db_pass.arn }
-    ]
-  })
-}
